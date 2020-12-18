26aee9ae8d1561ca7f1fb30d5a5e1b5ffc85d94a
neiliam <neil.tagger@gmail.com>
2020-12-18 20:47:25 +0200

    Created Unity project, added a ball and plane, simple camera follow script and simple force adding script for the ball when 'space' button is pressed.

---
+++ Assets/Scripts/BallTestScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallTestScript : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float power = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            print("powah!");
+            rb.AddForce(power,power,0);
+        }
+    }
+}
